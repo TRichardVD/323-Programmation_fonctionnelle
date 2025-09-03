d149c3874bc85b9fc057cd243bece88c8a349e6e
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-09-03 12:07:25 +0200

    [WIP] do exercise Cinema

---
+++ personnel/Cinema/CinemaApp/CinemaApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        private class Movie
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+
+            public double Rating { get; set; }
+            public int Year { get; set; }
+            public string[] LanguageOptions { get; set; }
+            public string[] StreamingPlatforms { get; set; }
+        }
+
+
+            static void Main(string[] args)
+        {
+            List<Movie> frenchMovies = new List<Movie>() {
+new Movie() { Title = "Le fabuleux destin d'Amélie Poulain", Genre = "Comédie", Rating = 8.3, Year = 2001, LanguageOptions = new string[] {"Français", "English"}, StreamingPlatforms = new string[] {"Netflix", "Hulu"} },
+new Movie() { Title = "Intouchables", Genre = "Comédie", Rating = 8.5, Year = 2011, LanguageOptions = new string[] {"Français"}, StreamingPlatforms = new string[] {"Netflix", "Amazon"} },
+new Movie() { Title = "The Matrix", Genre = "Science-Fiction", Rating = 8.7, Year = 1999, LanguageOptions = new string[] {"English", "Español"}, StreamingPlatforms = new string[] {"Hulu", "Amazon"} },
+new Movie() { Title = "La Vie est belle", Genre = "Drame", Rating = 8.6, Year = 1946, LanguageOptions = new string[] {"Français", "Italiano"}, StreamingPlatforms = new string[] {"Netflix"} },
+new Movie() { Title = "Gran Torino", Genre = "Drame", Rating = 8.2, Year = 2008, LanguageOptions = new string[] {"English"}, StreamingPlatforms = new string[] {"Hulu"} },
+new Movie() { Title = "La Haine", Genre = "Drame", Rating = 8.1, Year = 1995, LanguageOptions = new string[] {"Français"}, StreamingPlatforms = new string[] {"Netflix"} },
+new Movie() { Title = "Oldboy", Genre = "Thriller", Rating = 8.4, Year = 2003, LanguageOptions = new string[] {"Coréen", "English"}, StreamingPlatforms = new string[] {"Amazon"} }
+};
+
+            // Step 1
+            frenchMovies.Where(x => x.Genre == "Comédie" && x.Genre == "Drame");
+
+            // Step 2
+
+            // Step 3
+
+            // Step 4
+
+            // Step 5
+
+
+
+        }
+    }
+}
