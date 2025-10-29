45c1a128a70b89d6cba67bced1f84dfdd75cd4b7
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-10-29 10:03:16 +0100

    exercice Fibonacci : etape 1 -> 4

---
+++ personnel/Fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n <= 0) return 0;
+            else if (n == 1) return 1;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====== FIBONACCI ======");
+
+            for(int i = 0; i < 13; i++)
+            {
+                Console.Write(Fibonacci(i) + ", ");
+            }
+        }
+    }
+}
